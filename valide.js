03b5f08752e41d96001d0b34cc394f34716b689d
ilias anouar <ilas.anouar.solicode@gmail.com>
2022-11-19 15:23:25 +0100

    add requered

--- valide.js
+++ valide.js
@@ -1,4 +1,4 @@
-document.getElementById('form').onsubmit = function() {
+document.getElementById('form').onsubmit = function () {
     // input value
     let fname = document.getElementById('first-name').value;
     let lname = document.getElementById('last-name').value;
@@ -8,7 +8,7 @@ document.getElementById('form').onsubmit = function() {
     let valideEmail = /(\w+)?\.?(\w+)?\.?(\w+)?\d?\@(gmail|hotmail|yahoo|ofppt)\.(com|org|net|ma)/ig;
     let valideNumber = /(06|07|05)\d{8}/g;
     let valideName = /^(^[a-z]+['-\s]?[a-z]+)$/gi;
-    let valideLN =   /^(^[a-z]+['-\s]?[a-z]+)$/gi;
+    let valideLN = /^(^[a-z]+['-\s]?[a-z]+)$/gi;
     // test
     let valideN = valideName.test(fname);
     let valideLn = valideLN.test(lname);
@@ -20,31 +20,32 @@ document.getElementById('form').onsubmit = function() {
     console.log(validePhon);
     let e = true
     // condition
-if (e=true) {
-	    if ((valideN === false)||(fname === "")) {
-	        document.getElementById('f-name').style.display = "block"
-	    }else if (valideN === true) {
+    if (e = true) {
+        if ((valideN === false) || (fname === "")) {
+            document.getElementById('f-name').style.display = "block"
+        } else if (valideN === true) {
             document.getElementById('f-name').style.display = "none"
         }
-	    if((valideLn === false) || (lname === "")) {
-	        document.getElementById('l-name').style.display = "block"
-	    }else if(valideLn === true){
+        if ((valideLn === false) || (lname === "")) {
+            document.getElementById('l-name').style.display = "block"
+        } else if (valideLn === true) {
             document.getElementById('l-name').style.display = "none"
         }
-	    if ((validmail === false)||(email === "")) {
-	        document.getElementById('e-mail').style.display = "block"
-	    }else if(validmail === true){
+        if ((validmail === false) || (email === "")) {
+            document.getElementById('e-mail').style.display = "block"
+        } else if (validmail === true) {
             document.getElementById('e-mail').style.display = "none"
         }
-	    if ((validePhon === false)||(number === "")) {
-	        document.getElementById('number').style.display = "block"
-	    }else  if(validePhon === true){
+        if ((validePhon === false) || (number === "")) {
+            document.getElementById('number').style.display = "block"
+        } else if (validePhon === true) {
             document.getElementById('number').style.display = "none"
         }
         return false
-}else{
-    return true
-}
+    } else {
+        return true
+    }
+    // checkBox
 }
 
 

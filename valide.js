4cab9e905a97d8b4b47c59b1a81dd2c948456dc7
ilias anouar <ilas.anouar.solicode@gmail.com>
2022-11-19 14:46:08 +0100

    code optimisation

--- valide.js
+++ valide.js
@@ -1,14 +1,15 @@
-document.getElementById('form').onsubmit = function() {
+document.getElementById("form").onsubmit = function () {
     // input value
-    let fname = document.getElementById('first-name').value;
-    let lname = document.getElementById('last-name').value;
-    let email = document.getElementById('email').value;
-    let number = document.getElementById('phone').value;
+    let fname = document.getElementById("first-name").value;
+    let lname = document.getElementById("last-name").value;
+    let email = document.getElementById("email").value;
+    let number = document.getElementById("phone").value;
     // regEx
-    let valideEmail = /(\w+)?\.?(\w+)?\.?(\w+)?\d?\@(gmail|hotmail|yahoo|ofppt)\.(com|org|net|ma)/ig;
+    let valideEmail =
+      /(\w+)?.?(\w+)?.?(\w+)?\d?@(gmail|hotmail|yahoo|ofppt).(com|org|net|ma)/gi;
     let valideNumber = /(06|07|05)\d{8}/g;
-    let valideName = /^[a-z]*((['-])[a-z][a-z]*)*?$/gi;
-    let valideLN =  /^[a-z]*((['-])[a-z][a-z]*)*?$/gi;
+    let valideName = /^(^[a-z]+['-\s]?[a-z]+)$/gi;
+    let valideLN = /^(^[a-z]+['-\s]?[a-z]+)$/gi;
     // test
     let valideN = valideName.test(fname);
     let valideLn = valideLN.test(lname);
@@ -18,30 +19,31 @@ document.getElementById('form').onsubmit = function() {
     console.log(valideLn);
     console.log(validmail);
     console.log(validePhon);
+    let e = true;
     // condition
-if (e = true) {
-	    if ((valideN === false)||(fname === "")) {
-	        document.getElementById('f-name').style.display = "block"
-	    }else{
-            document.getElementById('f-name').style.display = "none"
-        }
-	    if ((valideLn === false) || (lname === "")) {
-	        document.getElementById('l-name').style.display = "block"
-	    }else{
-            document.getElementById('f-name').style.display = "none"
-        }
-	    if ((validmail === false)||(email === "")) {
-	        document.getElementById('e-mail').style.display = "block"
-	    }else{
-            document.getElementById('f-name').style.display = "none"
-        }
-	    if ((validePhon === false)||(number === "")) {
-	        document.getElementById('number').style.display = "block"
-	    }else{
-            document.getElementById('f-name').style.display = "none"
-        }
-        return false
-}else{
-    return true
-}
-}
+    if ((e = true)) {
+      if (valideN === false) {
+        document.getElementById("f-name").style.display = "block";
+      } else if (valideN === true) {
+        document.getElementById("f-name").style.display = "none";
+      }
+      if (valideLn === false) {
+        document.getElementById("l-name").style.display = "block";
+      } else if (valideLn === true) {
+        document.getElementById("l-name").style.display = "none";
+      }
+      if (validmail === false) {
+        document.getElementById("e-mail").style.display = "block";
+      } else if (validmail === true) {
+        document.getElementById("e-mail").style.display = "none";
+      }
+      if (validePhon === false) {
+        document.getElementById("number").style.display = "block";
+      } else if (validePhon === true) {
+        document.getElementById("number").style.display = "none";
+      }
+      return false;
+    } else {
+      return true;
+    }
+  };

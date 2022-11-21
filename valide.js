014f35e0676d013c4cd007dbb29472b50d4a6119
ilias-anouar <ilas.anouar.solicode@gmail.com>
2022-11-21 12:52:15 +0100

    all requered

--- valide.js
+++ valide.js
@@ -8,11 +8,12 @@ document.getElementById('form').onsubmit = function () {
     let genders = document.getElementsByName('Gender')
     let groups = document.getElementsByName('Group')
     // checkbox type
-    let allclub = document.getElementById('"allclub').getElementsByTagName('input')
+    let allclub = document.getElementById('allclub').getElementsByTagName('input')
     let limit = 3
     // check variabls
+
     let groupschecked = ((groups[0].checked == true)||(groups[1].checked == true)||(groups[2].checked == true)||(groups[3].checked == true)||(groups[4].checked == true));
-    let groupsnochecked = ((groups[0].checked == false)||(groups[1].checked == false)||(groups[2].checked == false)||(groups[3].checked == false)||(groups[4].checked == false));
+    let groupsnochecked = ((groups[0].checked == false)&&(groups[1].checked == false)&&(groups[2].checked == false)&&(groups[3].checked == false)&&(groups[4].checked == false));
     // regEx
     let valideEmail = /(\w+)?\.?(\w+)?\.?(\w+)?\d?\@(gmail|hotmail|yahoo|ofppt)\.(com|org|net|ma)/ig;
     let valideNumber = /(06|07|05)\d{8}/g;
@@ -46,45 +47,54 @@ document.getElementById('form').onsubmit = function () {
         // input value
         if ((valideN === false) || (fname === "")) {
             document.getElementById('f-name').style.display = "block"
+            document.getElementById('first-name').style.borderColor = "red"
         } else if (valideN === true) {
             document.getElementById('f-name').style.display = "none"
         }
         if ((valideLn === false) || (lname === "")) {
             document.getElementById('l-name').style.display = "block"
+            document.getElementById('last-name').style.borderColor = "red"
         } else if (valideLn === true) {
             document.getElementById('l-name').style.display = "none"
         }
         if ((validmail === false) || (email === "")) {
             document.getElementById('e-mail').style.display = "block"
+            document.getElementById('email').style.borderColor = "red"
         } else if (validmail === true) {
             document.getElementById('e-mail').style.display = "none"
         }
         if ((validePhon === false) || (number === "")) {
             document.getElementById('number').style.display = "block"
+            document.getElementById('phone').style.borderColor = "red"
         } else if (validePhon === true) {
             document.getElementById('number').style.display = "none"
         }
         // gender radio
-        if ((genders[0].checked == false)||(genders[1].checked == false)) {
+        if ((genders[0].checked == false)&&(genders[1].checked == false)) {
             document.getElementById('gender').style.display = "block"
+            document.getElementById('allgender').style.color = 'red'
         } else if ((genders[0].checked == true)||(genders[1].checked == false)){
             document.getElementById('gender').style.display = "none"
+            document.getElementById('allgender').style.color = 'green'
         }
-        // group radio
-        if (groupschecked) {
-            document.getElementById('group').style.display = "none"
-        }else if (groupsnochecked) {
+        // // group radio
+        if (groupsnochecked) {
             document.getElementById('group').style.display = "block"
+            document.getElementById('allgroup').style.color = 'red'
+        }else if (groupschecked) {
+            document.getElementById('group').style.display = "none"
+            document.getElementById('allgroup').style.color = 'green'
         }
         // checkbox
         if (allclub.checked == true) {
             document.getElementById('club').style.display = "none"
         }else{
             document.getElementById('club').style.display = "block"
+            document.getElementById('allclub').style.color = 'red'
         }
         return false
     } else {
-        return true
+        return document.getElementsByTagName('body').innerHTML = `<p>congratulation</p>`
     }
 }
 

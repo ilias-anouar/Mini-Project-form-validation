9514d3fa3ef4ca7135c34070bd43c7973ccbadf9
ilias-anouar <ilas.anouar.solicode@gmail.com>
2022-11-28 09:32:17 +0100

    v2

--- valide.js
+++ valide.js
@@ -4,12 +4,12 @@ function checkbox(elem) {
         if (elem[i].checked == true) {
             x++
         }
-        }
-        if (x>=1 && x<=3) {
-            return true
-        }else{
-            return false
-        }
+    }
+    if (x >= 1 && x <= 3) {
+        return true
+    } else {
+        return false
+    }
 }
 function checkP() {
     let p = document.querySelectorAll("#form p")
@@ -21,7 +21,7 @@ function checkP() {
     for (let i = 0; i < table.length; i++) {
         if (table[i].classList.contains("erore")) {
             return result = false
-        }  
+        }
     }
     return result
 }
@@ -37,8 +37,8 @@ function valide() {
     // checkbox type
     let allclub = document.getElementById('allclub').getElementsByTagName('input')
     // check variabls
-    let groupschecked = ((groups[0].checked == true)||(groups[1].checked == true)||(groups[2].checked == true)||(groups[3].checked == true)||(groups[4].checked == true));
-    let groupsnochecked = ((groups[0].checked == false)&&(groups[1].checked == false)&&(groups[2].checked == false)&&(groups[3].checked == false)&&(groups[4].checked == false));
+    let groupschecked = ((groups[0].checked == true) || (groups[1].checked == true) || (groups[2].checked == true) || (groups[3].checked == true) || (groups[4].checked == true));
+    let groupsnochecked = ((groups[0].checked == false) && (groups[1].checked == false) && (groups[2].checked == false) && (groups[3].checked == false) && (groups[4].checked == false));
     // regEx
     let valideEmail = /(\w+)?\.?(\w+)?\.?(\w+)?\d?\@(gmail|hotmail|yahoo|ofppt)\.(com|org|net|ma)/ig;
     let valideNumber = /(06|07|05)\d{8}/g;
@@ -82,10 +82,10 @@ function valide() {
             document.getElementById('phone').style.borderColor = "green"
         }
         // gender radio
-        if ((genders[0].checked == false)&&(genders[1].checked == false)) {
+        if ((genders[0].checked == false) && (genders[1].checked == false)) {
             document.getElementById('gender').classList.add("erore")
             document.getElementById('allgender').style.color = 'red'
-        } else if ((genders[0].checked == true)||(genders[1].checked == false)){
+        } else if ((genders[0].checked == true) || (genders[1].checked == false)) {
             document.getElementById('gender').classList.remove("erore")
             document.getElementById('allgender').style.color = 'green'
         }
@@ -93,7 +93,7 @@ function valide() {
         if (groupsnochecked) {
             document.getElementById('group').classList.add("erore")
             document.getElementById('allgroup').style.color = 'red'
-        }else if (groupschecked) {
+        } else if (groupschecked) {
             document.getElementById('group').classList.remove("erore")
             document.getElementById('allgroup').style.color = 'green'
         }
@@ -101,17 +101,17 @@ function valide() {
         if (checkbox(allclub)) {
             document.getElementById('club').classList.remove("erore")
             document.getElementById('allclub').style.color = 'green'
-        }else{
+        } else {
             document.getElementById('club').classList.add("erore")
             document.getElementById('allclub').style.color = 'red'
         }
         return false
     }
 }
-document.getElementById('send').onclick = function(e){
+document.getElementById('send').onclick = function (e) {
     valide();
     checkP();
     if (checkP() == true) {
         document.getElementById('confirmation').href = 'confirmation.html'
-}
+    }
 }
